d9eec274a91566a39d454d3ce21b3ae29a30b67f
OvidiuAndrei98 <apenica@iqnox.com>
2024-01-19 15:15:50 +0200

    update

--- src/App.js
+++ src/App.js
@@ -58,9 +58,25 @@ window.addEventListener("scroll", changeNavbarColor);
             <span className='title'>Seriozitate</span>
             <span className='subtitle'>Te poti baza pe noi</span>
           </div>
-        `</div>
+        </div>
       </div>
-      <footer></footer>
+      <div className='main-about'>
+        <div className='main-about-text'>
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Non odio euismod lacinia at quis. Volutpat diam ut venenatis tellus in metus vulputate eu scelerisque. Id leo in vitae turpis massa sed elementum tempus egestas. At augue eget arcu dictum. Gravida neque convallis a cras. Cursus eget nunc scelerisque viverra mauris in aliquam sem. Turpis massa sed elementum tempus egestas sed. Dictum at tempor commodo ullamcorper a lacus. Mauris a diam maecenas sed enim. Rhoncus urna neque viverra justo nec ultrices dui. Suscipit tellus mauris a diam maecenas sed enim. Turpis massa tincidunt dui ut ornare lectus. Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo. Dictum at tempor commodo ullamcorper a lacus vestibulum sed. Enim ut tellus elementum sagittis vitae et. Viverra nibh cras pulvinar mattis nunc.
+
+Venenatis tellus in metus vulputate eu scelerisque felis. At lectus urna duis convallis. Amet consectetur adipiscing elit duis tristique. </span>
+        </div>
+      </div>
+      <div className='section'>Demo section</div>
+      <footer className='footer'>
+        <div className='main-container'>
+          <div>Legal</div>
+          <div>Text</div>
+          <div>Text</div>
+          <div>Text</div>
+        </div>
+        <span className='credits'>Made by @andreipenica</span>
+      </footer>
     </>
   );
 }
